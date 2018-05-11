77208015136e58d761946d3a10ee0c37442b433c
pinesoft <1174504089@qq.com>
2018-05-11 13:33:43 +0800

    基础结构匡济0.1

---
+++ pinesoft/pinesoft-platform-pom/pinesoft-test-start/src/test/java/com/pinesoft/core/pinesoftteststart/PinesoftTestStartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pinesoft.core.pinesoftteststart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PinesoftTestStartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
