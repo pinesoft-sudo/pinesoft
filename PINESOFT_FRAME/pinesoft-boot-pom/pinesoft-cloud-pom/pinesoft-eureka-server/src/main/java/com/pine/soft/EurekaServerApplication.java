58e0e952e2abfba8aee2c4b07c2e1c927c7b6de1
pinesoft <1174504089@qq.com>
2018-05-29 09:50:59 +0800

    修改版本

--- PINESOFT_FRAME/pinesoft-boot-pom/pinesoft-cloud-pom/pinesoft-eureka-server/src/main/java/com/pine/soft/EurekaServerApplication.java
+++ PINESOFT_FRAME/pinesoft-boot-pom/pinesoft-cloud-pom/pinesoft-eureka-server/src/main/java/com/pine/soft/EurekaServerApplication.java
@@ -2,10 +2,10 @@ package com.pine.soft;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 //注解可以包括下面三个注解@Configuration@EnableAutoConfiguration@ComponentScan，但默认需要将该文件置于类的顶级位置
 @SpringBootApplication
@@ -15,4 +15,5 @@ public class EurekaServerApplication {
 		SpringApplication.run(EurekaServerApplication.class, args);
 		System.out.println("Eureka 注册中心已经开启");
 	}
+
 }
