832037603fc233aafe6a45fddfaee5ab8720060a
pinesoft <1174504089@qq.com>
2018-05-31 14:49:21 +0800

    ACTIVEMQ功能测试

---
+++ PINESOFT_FRAME/pinesoft-integration-pom/pinesoft-mq/src/test/java/org/pine/mq/test/Customer1.java
@@ -0,0 +1,19 @@
+package org.pine.mq.test;
+
+import org.pine.mq.active.MQConfig;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer1 {
+	@JmsListener(destination = MQConfig.DEFAULT_TOPIC_NAME)
+	public void receiveTopic(String text) {
+		System.out.println("customer-topic1收到的报文为:" + text);
+	}
+	
+	@JmsListener(destination = MQConfig.DEFAULT_QUEUE_NAME, containerFactory = MQConfig.QUEUE_LISTENER_CONTAINER_FACTORY)
+	// @SendTo("out.queue") 可以再转发另一个队列
+	public void receiveQueue(String text) {
+		System.out.println("customer-queue1收到的报文为:" + text);
+	}
+}
---
+++ PINESOFT_FRAME/pinesoft-integration-pom/pinesoft-mq/src/test/java/org/pine/mq/test/TestMQ.java
@@ -0,0 +1,36 @@
+package org.pine.mq.test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.pine.mq.DemoApplication;
+import org.pine.mq.active.MQProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class TestMQ {
+	@Autowired
+	private MQProducer mQProducer;
+
+	@Test
+	public void queueTest() throws InterruptedException, IOException {
+		for (int i = 0; i < 10; i++) {
+			mQProducer.sendQueueMessage("gisquest mq queue test"+i);
+		}
+	}
+
+	@Test
+	public void topicTest() throws InterruptedException, IOException {
+		for (int i = 0; i < 10; i++) {
+			mQProducer.sendTopicMessage("gisquest mq topic test"+i);
+			Thread.sleep(3000);
+		}
+
+	}
+}
