9a8c32d79a365c59367f7a1e6ae6c2ae55de007d
yangsong <yangsong@cmhi.chinamobile.com>
2018-06-29 16:15:56 +0800

    配置中心服务功能上传（非高可用）

--- PINESOFT_FRAME/pinesoft-boot-pom/pinesoft-cloud-pom/pinesoft-config-server/src/main/java/com/pine/soft/ConfigServerApplication.java
+++ PINESOFT_FRAME/pinesoft-boot-pom/pinesoft-cloud-pom/pinesoft-config-server/src/main/java/com/pine/soft/ConfigServerApplication.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 //注解可以包括下面三个注解@Configuration@EnableAutoConfiguration@ComponentScan，但默认需要将该文件置于类的顶级位置
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ConfigServerApplication.class, args);
