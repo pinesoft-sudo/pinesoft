e9fe4258f0e72b8a35d1b17c062d1b5363054cda
yangsong <yangsong@cmhi.chinamobile.com>
2018-06-29 14:21:57 +0800

    部分修改

--- PINESOFT_FRAME/pinesoft-boot-pom/pinesoft-cloud-pom/pinesoft-eureka-server/src/main/java/com/pine/soft/EurekaServerApplication.java
+++ PINESOFT_FRAME/pinesoft-boot-pom/pinesoft-cloud-pom/pinesoft-eureka-server/src/main/java/com/pine/soft/EurekaServerApplication.java
@@ -2,7 +2,6 @@ package com.pine.soft;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
